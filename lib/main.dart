2faeb892a5c51a562f59fefde63610b47d30bf92
tgthiag <tgthiag@gmail.com>
2023-03-29 23:10:53 -0300

    Table layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:io';
-import 'package:excel/excel.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:excel_to_json/excel_to_json.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,27 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -40,16 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -57,75 +31,132 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  List<String> rowdetail = [];
-
   _importFromExcel() async {
-    var file =
-        "C:\\Users\\eduar\\Downloads\\Dados ARC - CUMBICA.xlsx";
-    var bytes = File(file).readAsBytesSync();
-    var excel = Excel.decodeBytes(bytes);
+    String? excel = await ExcelToJson().convert();
+    var jsonfile = json.decode(excel!);
+    var rest = jsonfile["Banco de dados"];
 
+    final filteredData = rest.where((item) =>
+    item["Conclusão"] == "PROCEDENTE" && item["Ano"] > 2020 && item["Área responsável"] == "CONVERSÃO"
+    );
 
-      for (var row in excel.tables['Banco de dados']!.rows) {
-        print("${row.map((e) => e?.value)}");
-      }
-    }
-
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-
+    filteredData.forEach((item) {
+      print(item);
     });
-  }
+
+    }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.all(20),
+        child: Table(
+            columnWidths: {
+              0: FlexColumnWidth(1),
+              1: FlexColumnWidth(2),
+              2: FlexColumnWidth(2),
+              3: FlexColumnWidth(2),
+              4: FlexColumnWidth(2),
+            },
+            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+            border: TableBorder.all(width: 1.0, color: Colors.grey,borderRadius: const BorderRadius.all(Radius.circular(10))),
+            children: [
+              TableRow(
+                  decoration: BoxDecoration( color: Colors.grey, borderRadius: BorderRadius.vertical(top: Radius.circular(10))),
+                  children :[
+                Text('Setores',textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold)),
+                Text('Ultima reclamação:',textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold)),
+                Text('Recorde anterior\n sem reclamações:',textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text("Arc's procedentes\nno mês ",textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text("Arc's procedentes\nno ano ",textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold)),
+              ]),
+              TableRow(children :[
+                Padding(
+                    padding: EdgeInsets.all(4), //apply padding to all four sides
+                    child:
+                Text('Flap Disk', style: TextStyle(fontWeight: FontWeight.bold))),
+                Text('Dart',textAlign: TextAlign.center),
+                Text('Lars Bak',textAlign: TextAlign.center),
+                Text('Lars Bak',textAlign: TextAlign.center),
+                Text('Lars Bak',textAlign: TextAlign.center),
+              ]),
+              TableRow(children :[
+                Padding(
+                    padding: EdgeInsets.all(4), //apply padding to all four sides
+                    child:
+                Text('Folhas', style: TextStyle(fontWeight: FontWeight.bold))),
+                Text('Java',textAlign: TextAlign.center),
+                Text('James Gosling',textAlign: TextAlign.center),
+                Text('James Gosling',textAlign: TextAlign.center),
+                Text('Lars Bak',textAlign: TextAlign.center),
+              ]),
+              TableRow(children :[
+                Padding(
+                    padding: EdgeInsets.all(4), //apply padding to all four sides
+                    child:
+                Text('Cinta Larga', style: TextStyle(fontWeight: FontWeight.bold))),
+                Text('sdasdas',textAlign: TextAlign.center),
+                Text('Jamsadasdes Gosling',textAlign: TextAlign.center),
+                Text('Jamsadasdes Gosling',textAlign: TextAlign.center),
+                Text('Lars Bak',textAlign: TextAlign.center),
+              ]),
+              TableRow(children :[
+                Padding(
+                    padding: EdgeInsets.all(4), //apply padding to all four sides
+                    child:
+                Text('Cinta Estreita', style: TextStyle(fontWeight: FontWeight.bold))),
+                Text('Dart',textAlign: TextAlign.center),
+                Text('Lars Bak',textAlign: TextAlign.center),
+                Text('Lars Bak',textAlign: TextAlign.center),
+                Text('Lars Bak',textAlign: TextAlign.center),
+              ]),
+              TableRow(children :[
+                Padding(
+                    padding: EdgeInsets.all(4), //apply padding to all four sides
+                    child:
+                Text('Rolos', style: TextStyle(fontWeight: FontWeight.bold))),
+                Text('Java',textAlign: TextAlign.center),
+                Text('James Gosling',textAlign: TextAlign.center),
+                Text('James Gosling',textAlign: TextAlign.center),
+                Text('Lars Bak',textAlign: TextAlign.center),
+              ]),
+              TableRow(children :[
+                Padding(
+                    padding: EdgeInsets.all(4), //apply padding to all four sides
+                    child:
+                Text('Pluma', style: TextStyle(fontWeight: FontWeight.bold))),
+                Text('sdasdas',textAlign: TextAlign.center),
+                Text('Jamsadasdes Gosling',textAlign: TextAlign.center),
+                Text('Jamsadasdes Gosling',textAlign: TextAlign.center),
+                Text('Lars Bak',textAlign: TextAlign.center),
+              ]),
+              TableRow(children :[
+                Padding(
+                    padding: EdgeInsets.all(4), //apply padding to all four sides
+                    child:
+                Text('Fibra', style: TextStyle(fontWeight: FontWeight.bold))),
+                Text('sdasdas',textAlign: TextAlign.center),
+                Text('Jamsadasdes Gosling',textAlign: TextAlign.center),
+                Text('Jamsadasdes Gosling',textAlign: TextAlign.center),
+                Text('Lars Bak',textAlign: TextAlign.center),
+              ]),
+              TableRow(children :[
+                Padding(
+                    padding: EdgeInsets.all(4), //apply padding to all four sides
+                    child:
+                Text('SpeedLock', style: TextStyle(fontWeight: FontWeight.bold))),
+                Text('sdasdas',textAlign: TextAlign.center),
+                Text('Jamsadasdes Gosling',textAlign: TextAlign.center),
+                Text('Jamsadasdes Gosling',textAlign: TextAlign.center),
+                Text('Lars Bak',textAlign: TextAlign.center),
+              ]),
+            ]
         ),
       ),
       floatingActionButton: FloatingActionButton(
